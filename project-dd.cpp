9f8ab05588b3f4a4905a27e278c8aa5690c33b88
martinaarusconi <marti.rusca@gmail.com>
2026-01-19 18:11:12 +0000

    Add Partition

--- project-dd.cpp
+++ project-dd.cpp
@@ -14,6 +14,13 @@ using namespace Eigen;
 
 
 class Partition {
+    public:
+    static void compute_1d_partition(int N, int nb, int pid, int &start, int &len) {
+        int base = N / nb;                          // divide N point equally, the first "rest" processes get base+1 elements
+        int rest = N % nb;
+        start = pid * base + std::min(pid, rest);
+        len = base + (pid < rest ? 1 : 0);
+    }
 
 };
 
