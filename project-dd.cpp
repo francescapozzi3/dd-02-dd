4b5068679c10e323cb98226e23f0b1cdc26f922d
martinaarusconi <marti.rusca@gmail.com>
2026-01-19 18:10:09 +0000

    Add basic structure

---
+++ project-dd.cpp
@@ -0,0 +1,42 @@
+#include <mpi.h>
+#include <vector>
+#include <cmath>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <algorithm>
+
+#include <Eigen/Dense>
+#include <Eigen/LU>
+
+using namespace std;
+using namespace Eigen;
+
+
+class Partition {
+
+};
+
+
+class LocalProblem {
+
+};
+
+
+class SchwarzSolver {
+
+};
+
+
+int main(int argc, char** argv) {
+
+
+
+
+
+
+    return 0;
+}
+
+
+
